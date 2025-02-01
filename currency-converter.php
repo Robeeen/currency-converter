9b58cca2ca6ebcfd5738a1510d5f6cccea0dd4bb
Robeeen <robeeen@gmail.com>
2025-02-01 23:33:28 +0600

    Main php file

--- currency-converter.php
+++ currency-converter.php
@@ -1,2 +1,22 @@
 <?php
 
+/*
+Plugin Name: Currency Converter
+Description: A simple currency converter plugin for WordPress.
+Version: 1.0
+Author: Shams Khan
+*/
+
+function currency_converter_shortcode(){
+    ob_start(); ?>
+    <form id="currency-converter-form">
+        <label for="amount">Amount:</label>
+        <input type="number" id="amount" name="amount" step="0.01" required>
+
+
+
+    </form> 
+
+<?php
+}
+
