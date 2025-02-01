e79091c232c8cb0b70adf3926c670b4aae64aff4
Robeeen <robeeen@gmail.com>
2025-02-01 23:48:59 +0600

    Main php file

--- currency-converter.php
+++ currency-converter.php
@@ -11,11 +11,25 @@ function currency_converter_shortcode(){
     ob_start(); ?>
     <form id="currency-converter-form">
         <label for="amount">Amount:</label>
-        <input type="number" id="amount" name="amount" step="0.01" required>
+        <input type="number" id="amount" name="amount" step="0.01" required />
 
+        <label for="from_currency">From Currency: </label>
+        <select id="from_currency" name="from_currency" required>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
 
+        </select>   
 
+        <label for="to_currency">To Currency: </label>
+        <select id="to_currency" name="to_currency" required >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+        </select>
     </form> 
+
+    <div id="converted-result"></div>
 
 <?php
 }
